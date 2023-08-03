71369c9ed091f91e8c46a1fe9c4175e19da2a58d
GabiAlves74 <109996663+GabiAlves74@users.noreply.github.com>
2023-08-03 19:03:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup(){
   hypnoticBall = createSprite(250,250,15,15);
   hypnoticBall.shapeColor = "blue";
 
-  //metodo REF é usado para se referir a localização do valor do BD
+  //metodo REF é usado para referencia da localização do valor e/ou chave do BD
   var hypnoticBallPosition = database.ref('ball/position');
   // metodo ON é usado para acompanhar as mudanças feitas no BD
   hypnoticBallPosition.on("value", readPosition, showError);
@@ -36,15 +36,16 @@ function draw(){
   
 }
 
+//função para enviar dados para o DB
 function writePosition(x,y){
-  // instrução usada para definir valores que serão enviados para o banco de dados 
+  // instrução usada para definir valores que serão enviados para o banco de dados (SET)
   database.ref('ball/position').set({
     'x': position.x + x ,
     'y': position.y + y
   })
 }
 
-// função chamada quando ocorrem mudanças nos valores do BD
+// função para leitura de dados do DB
 function readPosition(data){
   // variável lê a posição do valor no BD e atribui as posições do objeto exibido
   position = data.val();
